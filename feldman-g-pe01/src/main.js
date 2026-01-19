07f399fcd941cc368e1b97f2e3b63837d03e4661
gfeldman120 <gfeldman120@gmail.com>
2026-01-19 13:54:42 -0500

    PE 1 - Technobabble

---
+++ feldman-g-pe01/src/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+console.log(words1[0]);
+
+// My code
+let text;
+let button;
+const wordsArrays = [words1, words2, words3];
+
+window.onload = () => {
+    text = document.querySelector(`#output`);
+    button = document.querySelector(`#myButton`);
+    button.addEventListener(`click`, makeText);
+}
+
+const makeText = () => {
+    let result = "";
+    for (let i = 0; i < wordsArrays.length; i++) {
+        result += `${wordsArrays[i][Math.floor(Math.random() * wordsArrays[i].length)]} `;
+    }
+    text.innerHTML = result;
+}
