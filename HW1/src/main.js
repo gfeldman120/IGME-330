893a99fdec20aae4a0b285e9c0608e8a095830f5
gfeldman120 <gfeldman120@gmail.com>
2026-02-05 14:23:03 -0500

    5 Babble

---
+++ HW1/src/main.js
@@ -0,0 +1,35 @@
+"use strict";
+
+const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
+
+const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
+
+const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platform", "Thruster", "Deflector", "Replicator", "Tricorder", "Operation", "Array", "Matrix", "Grid", "Sensor", "Mode", "Panel", "Storage", "Conduit", "Pod", "Hatch", "Regulator", "Display", "Inverter", "Spectrum", "Generator", "Cloud", "Field", "Terminal", "Module", "Procedure", "System", "Diagnostic", "Device", "Beam", "Probe", "Bank", "Tie-In", "Facility", "Bay", "Indicator", "Cell"];
+
+console.log(words1[0]);
+
+// My code
+let text;
+let button;
+let fiveButton;
+const wordsArrays = [words1, words2, words3];
+
+window.onload = () => {
+    text = document.querySelector(`#output`);
+    button = document.querySelector(`#myButton`);
+    fiveButton = document.querySelector(`#fiveButton`);
+    button.addEventListener(`click`, () => makeText(1));
+    fiveButton.addEventListener(`click`, () => makeText(5));
+    makeText(1);
+}
+
+const makeText = (count) => {
+    let result = "";
+    for (let i = 0; i < count; i++) {
+        for (let j = 0; j < wordsArrays.length; j++) {
+            result += `${wordsArrays[j][Math.floor(Math.random() * wordsArrays[j].length)]} `;
+        }
+        result += `<br>`;
+    }
+    text.innerHTML = result;
+}
