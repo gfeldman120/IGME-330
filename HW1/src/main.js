1a88441013e2b882f9b755f738e6a4478de5424e
gfeldman120 <gfeldman120@gmail.com>
2026-02-05 14:33:18 -0500

    Right before moving functions to other files

--- HW1/src/main.js
+++ HW1/src/main.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const words1 = ["Acute", "Aft", "Anti-matter", "Bipolar", "Cargo", "Command", "Communication", "Computer", "Deuterium", "Dorsal", "Emergency", "Engineering", "Environmental", "Flight", "Fore", "Guidance", "Heat", "Impulse", "Increased", "Inertial", "Infinite", "Ionizing", "Isolinear", "Lateral", "Linear", "Matter", "Medical", "Navigational", "Optical", "Optimal", "Optional", "Personal", "Personnel", "Phased", "Reduced", "Science", "Ship's", "Shuttlecraft", "Structural", "Subspace", "Transporter", "Ventral"];
 
 const words2 = ["Propulsion", "Dissipation", "Sensor", "Improbability", "Buffer", "Graviton", "Replicator", "Matter", "Anti-matter", "Organic", "Power", "Silicon", "Holographic", "Transient", "Integrity", "Plasma", "Fusion", "Control", "Access", "Auto", "Destruct", "Isolinear", "Transwarp", "Energy", "Medical", "Environmental", "Coil", "Impulse", "Warp", "Phaser", "Operating", "Photon", "Deflector", "Integrity", "Control", "Bridge", "Dampening", "Display", "Beam", "Quantum", "Baseline", "Input"];
@@ -8,28 +6,40 @@ const words3 = ["Chamber", "Interface", "Coil", "Polymer", "Biosphere", "Platfor
 
 console.log(words1[0]);
 
-// My code
+// Variables
 let text;
 let button;
 let fiveButton;
 const wordsArrays = [words1, words2, words3];
 
-window.onload = () => {
+// Get elements with querySelector, generate 1 technobabble
+const init = () => {
     text = document.querySelector(`#output`);
-    button = document.querySelector(`#myButton`);
-    fiveButton = document.querySelector(`#fiveButton`);
+    button = document.querySelector(`#btn-gen-1`);
+    fiveButton = document.querySelector(`#btn-gen-5`);
     button.addEventListener(`click`, () => generateTechno(1));
     fiveButton.addEventListener(`click`, () => generateTechno(5));
     generateTechno(1);
 }
 
+// Generate as many technobabbles as specified
 const generateTechno = (count) => {
     let result = "";
     for (let i = 0; i < count; i++) {
         for (let j = 0; j < wordsArrays.length; j++) {
-            result += `${wordsArrays[j][Math.floor(Math.random() * wordsArrays[j].length)]} `;
+            result += `${randomElement(wordsArrays[j])} `;
         }
         result += `<br>`;
     }
     text.innerHTML = result;
+}
+
+// Get a random element from an array
+const randomElement = (array) => {
+    return array[Math.floor(Math.random() * array.length)];
+}
+
+// Once all HTML has loaded
+window.onload = () => {
+    init();
 }
